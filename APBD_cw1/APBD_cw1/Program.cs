25b996536bc582f572a9d4bc4ed44c699e60ff5f
Karolina <61821714+s18705@users.noreply.github.com>
2020-10-23 12:46:43 +0200

    pierwsze cwiczenia

---
+++ APBD_cw1/APBD_cw1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace APBD_cw1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://www.pja.edu.pl");
+
+
+            if(response.IsSuccessStatusCode)
+            {
+                var html = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z.]+");
+
+                MatchCollection matches = regex.Matches(html);
+                foreach(var i in matches)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Koniec");
+        }
+    }
+}
