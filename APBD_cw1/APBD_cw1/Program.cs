a0f6912d132d195269dcbced2afe336cf6f45e4f
Karolina <61821714+s18705@users.noreply.github.com>
2020-10-31 12:00:25 +0100

    zadanie4

--- APBD_cw1/APBD_cw1/Program.cs
+++ APBD_cw1/APBD_cw1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,6 +10,17 @@ namespace APBD_cw1
     {
         public static async Task Main(string[] args)
         {
+            if(args.Length < 1)
+            {
+                throw new ArgumentNullException("args", "not enough arguments");
+            }
+
+            if(!jestPoprawnymURL(args[0]))
+            {
+                throw new ArgumentException("przekazany parametr nie jest poprawnym adresem URL");
+            }
+
+
             var httpClient = new HttpClient();
             var response = await httpClient.GetAsync("https://www.pja.edu.pl");
 
@@ -19,13 +31,30 @@ namespace APBD_cw1
                 var regex = new Regex("[a-z0-9]+@[a-z.]+");
 
                 MatchCollection matches = regex.Matches(html);
-                foreach(var i in matches)
+                foreach(var match in matches.OfType<Match>().Select(m => m.Value).Distinct())
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine(match);
                 }
-            }
 
-            Console.WriteLine("Koniec");
+                if(matches.Count == 0)
+                {
+                    Console.WriteLine("Nie znaleziono zadnego adresu email");
+                }
+
+                httpClient.Dispose();  
+            }
+            else
+            {
+                Console.WriteLine("Błąd wczasie pobierania strony");
+            }
+          
+           Console.WriteLine("Koniec");
+        }
+
+        public static bool jestPoprawnymURL(string url)
+        {
+            Uri uri;
+            return Uri.TryCreate(url, UriKind.Absolute, out uri) && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps);
         }
     }
 }
